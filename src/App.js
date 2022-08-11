442e3adc7b2368280dce82bc30321170ce03f1a3
gaurav <gaurav110498@gmail.com>
2022-08-12 00:04:43 +0530

    Search feature complete, onto fine tuning.

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import Pokemons from './components/pokemons/Pokemons';
 import Pagination from './components/pokemons/Pagination';
 import axios from 'axios';
 import Spinner from './components/layout/Spinner';
+import PokemonItem from './components/pokemons/PokemonItem';
 
 function App() {
   const [pokemons, setPokemons] = useState([]);
   const [pageUrl, setPageUrl] = useState ("https://pokeapi.co/api/v2/pokemon?limit=10" );
   const [searchName, setSearchName] = useState ([]);
+  const [selectedPokemon, setSelectedPokemon] = useState(false);
+
+
   // pagination 
   const [nextPage, setNextPage] = useState();
   const [prevPage, setPrevPage] = useState();
@@ -40,19 +44,24 @@ function App() {
     setPageUrl(prevPage);
   }
 
-  if(loading) return <Spinner />;
+  function searchPokemon() {
+    setSelectedPokemon(true);
+  }
 
+  if(loading) return <Spinner />;
+    
   return (
     <div className='app'>
       <div className='navbar'> 
         <h1>POKEMON FINDER</h1>
-        <input type='text' onChange={(e) => {setSearchName(e.target.value)}}/>
-        <button>Search</button>
+        <input type='text' onChange = {(e) => {setSearchName(e.target.value)}}/>
+        <button onClick={searchPokemon}>Search</button>
       </div>
-      
+      <div className='searchedpkmns'>
+        {!selectedPokemon ? (<h3>Please select a pokemon</h3>) : (<PokemonItem pokemon={searchName} />)} 
+      </div>
       <div className='paginatedpkmns'>
         <Pokemons pokemons={pokemons}/>
-        
         <Pagination 
           gotoNext={nextPage ? gotoNext : null}
           gotoPrev={prevPage ? gotoPrev : null}
